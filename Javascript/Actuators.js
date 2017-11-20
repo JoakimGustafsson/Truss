0aa28b70e7fb9b82f57ed437870972af205145d2
Joakim Gustafsson <joakim.h.gustafsson@se.atlascopco.com>
2017-11-20 09:58:59 +0100

    Made the actuators work with ES6 classes

--- Javascript/Actuators.js
+++ Javascript/Actuators.js
@@ -1,218 +1,177 @@
-/**
- * x
- */
-
-
-
-
-function ActuatorNode(obj, startPosition, mass=0.001,name="actuatornode", positionFunction, showFunction,velocityLoss=0.99){
-	Node.call(this, startPosition,mass,name, positionFunction, showFunction, velocityLoss);
-	this.iO=obj;	// the influenced object
-
+class ActuatorNode extends Node {
+	constructor(obj, startPosition, mass = 0.001, name = "actuatornode", positionFunction, showFunction, velocityLoss = 0.99) {
+		super(startPosition, mass, name, positionFunction, showFunction, velocityLoss);
+		this.iO = obj; // the influenced object
+	}
 }
-inheritPrototype(ActuatorNode, Node);
 
-function BinaryActuatorNode(obj,position1,position2,mass=0.001,name="binarynode", positionFunction, showFunction,velocityLoss=0.99){
-	ActuatorNode.call(this, obj,new Position(position1.x, position1.y), mass,name, positionFunction, showFunction,velocityLoss);
-	this.position1=position1;
-	this.position2=position2;
-	this.vector=subtractVectors(position2,position1);
-	
-
-	BinaryActuatorNode.prototype.getState = function(){	
-		if (boxClose(this.getPosition(),this.position1, 0.01))
+class BinaryActuatorNode extends ActuatorNode {
+	constructor(obj, position1, position2, mass = 0.001, name = "binarynode", positionFunction, showFunction, velocityLoss = 0.99) {
+		super(obj, new Position(position1.x, position1.y), mass, name, positionFunction, showFunction, velocityLoss);
+		this.position1 = position1;
+		this.position2 = position2;
+		this.vector = subtractVectors(position2, position1);
+	}
+	getState() {
+		if (boxClose(this.getPosition(), this.position1, 0.01))
 			return 1;
 		else if (boxClose(this.getPosition(), this.position2, 0.01))
 			return 2;
-		else return 0;
-	}
-	
-	BinaryActuatorNode.prototype.getAcceleration = function(forceAppliers){	
-		var tempAcceleration = Node.prototype.getAcceleration.call(this,forceAppliers);	//Call parent in order to update this nodes normal acceleration
-		
-		var acceleration = multiplyVector(	
-				dotProduct(this.vector, tempAcceleration),
-				divideVector(this.vector, length2(this.vector)));
-		return acceleration;	
-	}
-	
-	BinaryActuatorNode.prototype.updatePosition = function(time){
-		Node.prototype.updatePosition.call(this,time);
-		var fractionPosition = getT(this.position1,this.position2,this.getPosition());
-		if (fractionPosition<0) {
-			this.setPosition(new Position(this.position1.x,this.position1.y));
-			this.velocity = new Velocity(0,0);
+		else
+			return 0;
+	};
+	getAcceleration(forceAppliers) {
+		var tempAcceleration = Node.prototype.getAcceleration.call(this, forceAppliers); //Call parent in order to update this nodes normal acceleration
+		var acceleration = multiplyVector(dotProduct(this.vector, tempAcceleration), divideVector(this.vector, length2(this.vector)));
+		return acceleration;
+	};
+
+	updatePosition(time) {
+		super.updatePosition(time);
+		var fractionPosition = getT(this.position1, this.position2, this.getPosition());
+		if (fractionPosition < 0) {
+			this.setPosition(new Position(this.position1.x, this.position1.y));
+			this.velocity = new Velocity(0, 0);
+		} else if (fractionPosition > 1) {
+			this.setPosition(new Position(this.position2.x, this.position2.y));
+			this.velocity = new Velocity(0, 0);
 		}
-		else if (fractionPosition>1){
-			this.setPosition(new Position(this.position2.x,this.position2.y));
-			this.velocity = new Velocity(0,0);
+	};
+
+}
+
+
+
+class JumpNode extends ActuatorNode {
+	constructor(obj, position1, position2, gravityField, mass = 0.01, name = "jumpnode", positionFunction, showFunction, velocityLoss) {
+		super(obj, position1, position2, mass, name, positionFunction, showFunction, velocityLoss);
+		this.gravityField = gravityField;
+		this.originalGravityConstant = gravityField.constant;
+	}
+
+
+	updatePosition(time) {
+		//super.updatePosition(time); //Call parent in order to update this.iO nodes position
+
+		if (this.getState() == 1) {
+			this.gravityField.constant = this.originalGravityConstant;
+		} else if (this.getState() == 2) {
+			this.gravityField.constant = this.originalGravityConstant * 4;
 		}
 	}
 }
-inheritPrototype(BinaryActuatorNode, ActuatorNode);
 
-
-//startPosition,mass=1,name="node", positionFunction, showFunction, velocityLoss=1
-function JumpNode(obj, position1, position2, gravityField, mass=0.01,name="jumpnode", positionFunction, showFunction, velocityLoss){
-	BinaryActuatorNode.call(this, obj, position1, position2, mass,name, positionFunction, showFunction, velocityLoss);
-
-	this.gravityField = gravityField;
-	this.originalGravityConstant = gravityField.constant;
-	
-	JumpNode.prototype.updatePosition = function(time){
-		BinaryActuatorNode.prototype.updatePosition.call(this,time);	//Call parent in order to update this.iO nodes position
-
-		if (this.getState()==1){
-			this.gravityField.constant=this.originalGravityConstant;
-		}
-		else if (this.getState()==2){
-			this.gravityField.constant=this.originalGravityConstant*4;
-		}
+class SpringDanglerNode extends BinaryActuatorNode {
+	constructor(obj, position1, position2, rightMovementTensor, leftMovementTensor, mass = 0.01, name = "springrunnernode", positionFunction, showFunction, velocityLoss = 0.99) {
+		super(obj, position1, position2, mass, name, positionFunction, showFunction, velocityLoss);
+		this.currentTensors = [];
+		this.truss;
+		this.speed = 6.67e-11;
+		this.rightMovementTensor = rightMovementTensor;
+		this.leftMovementTensor = leftMovementTensor;
 	}
-}
-inheritPrototype(JumpNode, BinaryActuatorNode);
 
-//startPosition,mass=1,name="node", positionFunction, showFunction, velocityLoss=1
-function SpringDanglerNode(obj, position1, position2, rightMovementTensor, leftMovementTensor, mass=0.01,name="springrunnernode", positionFunction, showFunction, velocityLoss=0.99){
-	BinaryActuatorNode.call(this, obj, position1, position2, mass,name, positionFunction, showFunction, velocityLoss);
-
-	this.currentTensors =[];
-	this.truss;
-	this.speed=6.67e-11;
-	this.rightMovementTensor = rightMovementTensor;
-	this.leftMovementTensor = leftMovementTensor;
-	
-
-	SpringDanglerNode.prototype.attachToTruss = function(result){
+	attachToTruss(result) {
 		this.truss.addTensor(result.rightTensor);
 		this.truss.addTensor(result.leftTensor);
 		this.truss.ghostifyTensor(result.originalTensor);
 	}
-	SpringDanglerNode.prototype.detachFromTruss = function(result){
+	detachFromTruss(result) {
 		this.truss.removeTensor(result.rightTensor);
 		this.truss.removeTensor(result.leftTensor);
 		this.truss.deghostifyTensor(result.originalTensor);
 	}
-
-	SpringDanglerNode.prototype.attachToTruss = function(result){
+	attachToTruss(result) {
 		this.truss.addTensor(result.rightTensor);
 		this.truss.addTensor(result.leftTensor);
 		this.truss.ghostifyTensor(result.originalTensor);
 	}
-	
-	SpringDanglerNode.prototype.attachToTensor = function(truss, tensor, distanceFraction=0.5,dir=-1){
-		this.truss=truss;
-		var result={};
-		var rightNode=tensor.getRightNode();
-		var leftNode=tensor.getOppositeNode(rightNode);
-		if (tensor.node2==leftNode){
-			distanceFraction=1-distanceFraction;
+	attachToTensor(truss, tensor, distanceFraction = 0.5, dir = -1) {
+		this.truss = truss;
+		var result = {};
+		var rightNode = tensor.getRightNode();
+		var leftNode = tensor.getOppositeNode(rightNode);
+		if (tensor.node2 == leftNode) {
+			distanceFraction = 1 - distanceFraction;
 		}
-		result.rightTensor= new PullSpring(this.iO,rightNode,tensor.constant,tensor.equilibriumLength*(1-distanceFraction));
-		result.leftTensor= new PullSpring(leftNode,this.iO,tensor.constant,tensor.equilibriumLength*(distanceFraction));
-		result.originalTensor= tensor;
-		result.direction=dir;
+		result.rightTensor = new PullSpring(this.iO, rightNode, tensor.constant, tensor.equilibriumLength * (1 - distanceFraction));
+		result.leftTensor = new PullSpring(leftNode, this.iO, tensor.constant, tensor.equilibriumLength * (distanceFraction));
+		result.originalTensor = tensor;
+		result.direction = dir;
 		this.currentTensors.push(result);
 		this.attachToTruss(result);
 	}
-	
-	SpringDanglerNode.prototype.updatePosition = function(time){
-		BinaryActuatorNode.prototype.updatePosition.call(this,time);	//Call parent in order to update this.iO nodes position
 
+	updatePosition(time) {
+		super.updatePosition(time); //Call parent in order to update this.iO nodes position
 		var shorten;
 		var lengthen;
 		var direction;
-		var cleanupList=[];
-	
+		var cleanupList = [];
 		//***************************************************************
-		this.rightMovementTensor.constant=0;
-		this.leftMovementTensor.constant=0;
-		
-		for(var i=0; i<this.currentTensors.length; i++){
-
-			if (this.getState()==1){
-				this.rightMovementTensor.constant=this.speed;
+		this.rightMovementTensor.constant = 0;
+		this.leftMovementTensor.constant = 0;
+		for (var i = 0; i < this.currentTensors.length; i++) {
+			if (this.getState() == 1) {
+				this.rightMovementTensor.constant = this.speed;
+			} else if (this.getState() == 2) {
+				this.leftMovementTensor.constant = this.speed;
 			}
-			else if (this.getState()==2){
-				this.leftMovementTensor.constant=this.speed;
-			}
-			
-			
 			var TensorMap = this.currentTensors[i];
-			
-			TensorMap.rightTensor.equilibriumLength=
-				(TensorMap.originalTensor.equilibriumLength+
-				TensorMap.rightTensor.getLength()-
-				TensorMap.leftTensor.getLength())/2;
-			
+			TensorMap.rightTensor.equilibriumLength =
+				(TensorMap.originalTensor.equilibriumLength +
+					TensorMap.rightTensor.getLength() -
+					TensorMap.leftTensor.getLength()) / 2;
 			// calculate left equilibriumLength	
-			TensorMap.leftTensor.equilibriumLength=
-				TensorMap.originalTensor.equilibriumLength-
+			TensorMap.leftTensor.equilibriumLength =
+				TensorMap.originalTensor.equilibriumLength -
 				TensorMap.rightTensor.equilibriumLength;
-			
-			
 			var p1 = TensorMap.originalTensor.node1.getPosition();
 			var p2 = TensorMap.originalTensor.node2.getPosition();
 			var p3 = this.iO.getPosition();
-		
-//			Rewrite this for handling more than one ball...
-//			it should not be originaltensor but left right tensor.
-			
+			//			Rewrite this for handling more than one ball...
+			//			it should not be originaltensor but left right tensor.
 			//compare left and right angle and with respect to direction.
-
 			var perpendicularDistance = getS(p1, p2, p3);
-			var above = (perpendicularDistance*TensorMap.direction>0);
+			var above = (perpendicularDistance * TensorMap.direction > 0);
 			var inside = getTInside(p1, p2, p3);
-			var closeParallell = (Math.abs(perpendicularDistance)<0.2);
-			if (above && inside){
-				console.log("Bounce disconnected from "+TensorMap.originalTensor.getName());
+			var closeParallell = (Math.abs(perpendicularDistance) < 0.2);
+			if (above && inside) {
+				console.log("Bounce disconnected from " + TensorMap.originalTensor.getName());
 				this.disconnect(TensorMap);
 				cleanupList.push(TensorMap);
 				TensorMap.originalTensor.resetCollision(this.iO);
-			} else if (closeParallell && !inside){
-				console.log("Endpoint disconnected from "+TensorMap.originalTensor.getName());
+			} else if (closeParallell && !inside) {
+				console.log("Endpoint disconnected from " + TensorMap.originalTensor.getName());
 				this.disconnect(TensorMap);
 				cleanupList.push(TensorMap);
-				TensorMap.originalTensor.resetCollision(this.iO);	
+				TensorMap.originalTensor.resetCollision(this.iO);
 				// add 0.5 m above the exit node to represent that you can actually lift your knees when exiting a spring
-				this.iO.getPosition().add(
-						normalizeVector(
-								0.2,							//2 dm 
-								multiplyVector(
-										TensorMap.direction,	//wrt to the collision direction
-										perpendicular(TensorMap.originalTensor.getActual())	//above
-								)
-						)
-				);
+				this.iO.getPosition().add(normalizeVector(0.2, //2 dm 
+					multiplyVector(TensorMap.direction, //wrt to the collision direction
+						perpendicular(TensorMap.originalTensor.getActual()) //above
+					)));
 			}
 		}
-		for(j=0; j<cleanupList.length; j++){
-			removeIfPresent(cleanupList[j],this.currentTensors);
+		for (var j = 0; j < cleanupList.length; j++) {
+			removeIfPresent(cleanupList[j], this.currentTensors);
 		}
 	}
 
-	SpringDanglerNode.prototype.disconnect = function(TensorMap){
+	disconnect(TensorMap) {
 		this.detachFromTruss(TensorMap);
 	}
-	
-	SpringDanglerNode.prototype.exit = function(zeroNode, direction){
-		var newTensor = zeroNode.findTopSpring(direction,[this.iO.currentTensor,this.iO.mySpring]);
-		
+
+	exit(zeroNode, direction) {
+		var newTensor = zeroNode.findTopSpring(direction, [this.iO.currentTensor, this.iO.mySpring]);
 		this.iO.disconnect();
-		
-		if (!newTensor)							// No new spring anywhere forward
+		if (!newTensor)
 			return;
-
 		// Attach to new tensor
-		if (direction>0)
-			this.iO.attachToTensor(newTensor,0.02);
+		if (direction > 0)
+			this.iO.attachToTensor(newTensor, 0.02);
 		else
-			this.iO.attachToTensor(newTensor,0.98);
+			this.iO.attachToTensor(newTensor, 0.98);
 	}
+
 }
-inheritPrototype(SpringDanglerNode, BinaryActuatorNode);
-
-
-
-
-
--- Javascript/ExperimentalTrusses.js
+++ Javascript/ExperimentalTrusses.js
@@ -78,14 +78,6 @@ function WalkTruss(view, updatefrequency){
 	this.addTensor(new Absorber(f4, b4, absorbconstant));
 	this.addTensor(new Absorber(f5, b5, absorbconstant));
 
-	//ego.trussConnect(this);
-
-	//this.addCollider(ego);
-	
-//	this.initConnect();
-	
-	//ego.attachToTensor(startTensor,0.2);
-	
 
 	var sensorNode = this.addNode(new KeySensorNode(new Position(2,1),0.01,"myKeySensorNode"));
 	sensorNode.registerKey(37, new Vector(-1,0));
@@ -104,45 +96,30 @@ function WalkTruss(view, updatefrequency){
 			new SpringDanglerNode(ego1, new Position(1,0.5), new Position(3,0.5),
 					leftField1, rightField1,0.05,"mySpringDanglerNode",0,0,0.99));
 
-	var actuatorNode2 = this.addNode(
+/*	var actuatorNode2 = this.addNode(
 			new SpringDanglerNode(ego2, new Position(1,0.25), new Position(3,0.25),
 					leftField1, rightField1,0.05,"mySpringDanglerNode2",0,0,0.99));
-
+*/
 	var KeyDangleSpring = this.addTensor(new Spring(sensorNode, actuatorNode1, 50,0.1));
 	
-	
+/*	
 	var jumpActuator = this.addNode(
 			new JumpNode(ego1, new Position(0.5,1), new Position(0.5,2),
 					egoGravityField,0.05,"myJumpNode"));
 
 	var KeyJumpSpring = this.addTensor(new Spring(sensorNode, jumpActuator, 50,0.1));
-
+*/
 	var collissionsensor1 = new CollisionSensorNode(new Position(4,1),0.01,"CollissionSensor1");
 	collissionsensor1.registerTrussObjectAndActuator(this,ego1, actuatorNode1);
-	
+/*	
 	var collissionsensor2 = new CollisionSensorNode(new Position(8,1),0.01,"CollissionSensor2");
 	collissionsensor2.registerTrussObjectAndActuator(this,ego2, actuatorNode2);
-	
+*/	
 	
 	
 }
 inheritPrototype(WalkTruss, Truss);
 
-
-/*function gameLoop() {
-    if (keyState[37] || keyState[65]){
-        x -= 1;
-    }    
-    if (keyState[39] || keyState[68]){
-        x += 1;
-    }
-
-    // redraw/reposition your object here
-    // also redraw/animate any objects not controlled by the user
-
-    setTimeout(gameLoop, 10);
-}    
-gameLoop();*/
 
 function ProtagonistNode(startPosition, mass=70, name="ProtagonistNode", positionFunction, showFunction, velocityLoss=1){
 	Node.call(this, startPosition,mass,name, positionFunction, showFunction, 1);
@@ -150,119 +127,6 @@ function ProtagonistNode(startPosition, mass=70, name="ProtagonistNode", positio
 	this.actuators=0;
 	this.sensors=0;
 	
-/*	this.currentTensor=0;
-	this.currentTensorOriginalLength=0;
-	this.speed=0.05;
-	this.mySpring = new Spring(new Node(new Position(),name="Initial Spring Node"),this);
-	this.mySpring.equilibriumLength=3;
-	
-	
-	ProtagonistNode.prototype.attachToTensor = function(tensor,distanceFraction=0.5){
-		this.currentTensor=tensor;
-		this.mySpring.constant=tensor.constant;
-		this.currentTensorOriginalLength = tensor.equilibriumLength;
-		
-		this.rightNode = this.currentTensor.getRightNode();
-		this.mySpring.addNode1(this.rightNode);
-		this.currentTensor.setRightNode(this);
-		
-		this.mySpring.equilibriumLength = this.currentTensorOriginalLength*(1-distanceFraction);
-		this.currentTensor.equilibriumLength = this.currentTensorOriginalLength*(distanceFraction);
-			
-	}
-
-	// Add myself and my spring to this truss
-	ProtagonistNode.prototype.trussConnect = function(truss){
-		truss.addTensor(this.mySpring);
-		truss.addGravityNode(this);
-	}
-	
-	ProtagonistNode.prototype.updatePosition = function(time){
-		var shorten;
-		var lengthen;
-		var direction;
-	
-		//***************************************************************
-		
-		function modifyTensors(speed,length){
-			if (lengthen.equilibriumLength<0){
-				lengthen.equilibriumLength+=speed;
-				return;
-			}
-			shorten.equilibriumLength -= speed;
-			lengthen.equilibriumLength = Math.min(length, length-shorten.equilibriumLength);
-		}
-		function getOtherNode(tensor, node){
-			if (node==tensor.node1) 
-				return tensor.node2;
-			else
-				return tensor.node1;
-		}
-		function close(pos1,pos2, velocity){
-			//if ((pos1.x==pos2.x) && (pos1.y==pos2.y))
-			var d=boxLength(velocity);
-			if (Math.abs(pos1.x-pos2.x)<d && Math.abs(pos1.y-pos2.y)<d) 
-				return true;
-			return false;
-		}
-		
-		//***************************************************************
-		
-		Node.prototype.updatePosition.call(this,time);	//Call parent in order to update this nodes position
-
-		if (keyState[39] || keyState[68]){
-			shorten=this.mySpring;	
-			lengthen=this.currentTensor;	
-			direction=this.speed;
-		} 
-		else if (keyState[37] || keyState[65]){
-			shorten=this.currentTensor;
-			lengthen=this.mySpring;
-			direction=-this.speed;
-		}		
-		else
-			return;
-		
-		modifyTensors(this.speed,this.currentTensorOriginalLength);
-
-		var p1 = this.currentTensor.getOppositeNode(this).getPosition();
-		var p2 = this.rightNode.getPosition();
-		var p3 = this.getPosition();
-		
-		if (getS(p1, p2, p3)<0)
-			this.disconnect(getOtherNode(shorten,this));
-		
-		
-		
-		var zeroNode=getOtherNode(shorten,this);
-		if (zeroNode && close(this.getPosition(),zeroNode.getPosition(), this.velocity))
-		{
-			this.exit(zeroNode,direction);
-		}
-	}
-
-	ProtagonistNode.prototype.disconnect = function(lastNode){
-		this.mySpring.constant=0; 
-		// restore the old tensor
-		this.currentTensor.exchangeNode(this,this.rightNode);
-		this.currentTensor.equilibriumLength=this.currentTensorOriginalLength;
-	}
-	
-	ProtagonistNode.prototype.exit = function(zeroNode, direction){
-		var newTensor = zeroNode.findTopSpring(direction,[this.currentTensor,this.mySpring]);
-		
-		this.disconnect();
-		
-		if (!newTensor)							// No new spring anywhere forward
-			return;
-
-		// Attach to new tensor
-		if (direction>0)
-			this.attachToTensor(newTensor,0.02);
-		else
-			this.attachToTensor(newTensor,0.98);
-	}
-*/
 	ProtagonistNode.prototype.show = function(v, time, graphicDebugLevel=0){
 		if (v.inside(this.getPosition())) {
 				v.context.strokeStyle = "yellow"; 
@@ -342,117 +206,6 @@ inheritPrototype(MyTruss, Truss);
 
 
 
-
-
-/*
-function MyTruss(ctx, updatefrequency){
-	Truss.call(this, ctx, updatefrequency);
-	
-	var myX=3;
-	var myY=3;
-	var mouseSet=false;
-
-	function myMove(e) {
-		myX=e.pageX;
-		myY=e.pageY;
-	}
-
-	function downMouse(e) {
-		mouseSet=true;
-	}
-
-	function upMouse(e) {
-		mouseSet=false;
-	}
-
-	ctx.canvas.onmousemove = myMove;
-	ctx.canvas.onmousedown = downMouse;
-	ctx.canvas.onmouseup = upMouse;
-
-	var thisTrussView=this.view;
-
-var n1 = this.addNode(new Node(new Position(1, 0.3), 5, "fulcrum", function(node, tick){
-	return new Position(1+0.5*Math.sin(tick),0.3)
-	})); 
-var n2 = this.addNode(new Node(new Position(1.3, 0.6), 10, "right")); 
-var n3 = this.addNode(new Node(new Position(1, 0.9), 100, "down", function(node, n){
-	if (mouseSet)
-		return thisTrussView.worldPosition(myX, myY);
-	return node.getPosition();
-	})); 
-var n4 = this.addNode(new Node(new Position(0.7, 0.6), 10, "left")); 
-
-this.addTensor(new Spring(n1, n2, 100));
-this.addTensor(new Spring(n2, n3, 500));
-this.addTensor(new Spring(n3, n4, 500));
-this.addTensor(new Spring(n4, n1, 100));
-this.addTensor(new Spring(n2, n4, 100));
-
-
-this.addTensor(new Absorber(n1, n2, 100));
-this.addTensor(new Absorber(n2, n3, 249));
-this.addTensor(new Absorber(n3, n4, 500));
-this.addTensor(new Absorber(n4, n1, 500));
-this.addTensor(new Absorber(n2, n4, 100));
-
-this.addTensor(new gravityField(n2));
-this.addTensor(new gravityField(n3));
-this.addTensor(new gravityField(n4));
-
-this.initConnect();
-}
-
-inheritPrototype(MyTruss, Truss);
-*/
-
-/*
-Construction
-1.	Homebase
-2.	Lab
-2.	Springgenerator
-3.	Absorber
-4.	Killer
-5.	Searchtool
-5.	Fieldgenerator
-5.	Identityconstruct with pictures
-6.	RewardGenerator
-7.	Mob
-6.	Cannon	
-7.	Text
-8.	Web reference
-9.	Picture applier
-10.	Picture uploader
-11.	Store
-12.	Market
-
-Actuators
-1.	Springrunner
-2.	Jumper
-3.	Winethrower
-4.	Thruster (Interplanetary)
-5.	Thruster (Planetary)
-6.	Scroller/offsetter
-7.	Zoomer/scaler
-6.	Settings
-
-Sensors
-1.	Keysensor
-2.	Mousesensor
-3.	Eventlistner
-
-
-
-
-
-
-
-
-
-
-
-
- 
- */
 
 
 
